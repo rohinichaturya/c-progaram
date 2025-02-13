b34c71ff4ebe742e880465ca28f1ac66f7243b12
Yeturi Rohini Chaturya <147597602+rohinichaturya@users.noreply.github.com>
2025-02-13 10:30:35 +0530

    Update sum of digits.c

--- sum of digits.c
+++ sum of digits.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int num, count = 0;
+    int num, sum = 0, remainder;
     printf("Enter an integer: ");
     scanf("%d", &num);
 
     while (num != 0) {
+        remainder = num % 10;
+        sum += remainder;
         num /= 10;
-        count++;
     }
 
-    printf("Number of digits: %d\n", count);
+    printf("Sum of digits: %d\n", sum);
     return 0;
 }
