646257e45acff7e9b0d43149c884ea9f03f971f7
Yeturi Rohini Chaturya <147597602+rohinichaturya@users.noreply.github.com>
2025-02-13 10:18:56 +0530

    armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num, original, remainder, n = 0, result = 0;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    
+    original = num;
+    while (original != 0) 
+{
+        original /= 10;
+        ++n;
+    }
+
+    original = num;
+    while (original != 0) 
+{
+        remainder = original % 10;
+        result += pow(remainder, n);
+        original /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
